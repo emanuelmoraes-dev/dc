abf94d36eb4cc49627e30296ac0325d17f6b47c9
Emanuel Moraes <emanuelmoraes297@gmail.com>
2023-12-08 00:28:01 -0300

    content_sentence.c renamed to content_alphabet.c

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -3,7 +3,7 @@ add_library(
     SHARED
         src/lib/content/content_init.c
         src/lib/content/content_free.c
-        src/lib/content/content_sentence.c
+        src/lib/content/content_alphabet.c
         src/lib/content/content_odds.c
 )
 
