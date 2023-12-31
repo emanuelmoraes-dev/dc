43e0de424d69fb63f63a5c4ee408de7bd693339c
Emanuel Moraes <emanuelmoraes297@gmail.com>
2023-12-31 14:41:55 -0300

    bugfix free memory if dcl_content_init throws error

--- src/dcl/content/content_init.c
+++ src/dcl/content/content_init.c
@@ -9,14 +9,13 @@ c_err dcl_content_init(DclContent* content, int alphabet_size, int keys_size) {
 	c_err error = uc_hash_map_init(&content->alphabet, capacity);
 
 	if (error != C_OK) {
-		dcl_content_free(content);
 		return error;
 	}
 
 	error = uc_hash_map_init(&content->odds_graph, keys_size);
 
 	if (error != C_OK) {
-		dcl_content_free(content);
+		uc_hash_map_free(&content->alphabet, NULL);
 		return error;
 	}
 
